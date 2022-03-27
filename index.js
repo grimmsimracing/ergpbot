115339dea6ca9cc29d8eac522faf30ea89972cca
grimmsimracing <80625695+grimmsimracing@users.noreply.github.com>
2022-03-27 12:06:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const prefix = '!';
 const client = new Discord.Client();
 var globalEventID = '';
 
-client.once('ready', () => {
-    console.log('bot is ready');
+client.on('ready', () => {
+    console.log('Logged in...');
 });
 
 client.on('message', (msg) => {
 
     const driver_username = msg.member.user.username+"#"+msg.member.user.discriminator;
+    //if(msg.author.bot) return
     if(!msg.content.startsWith(prefix)) return
     const channelID = msg.channel.topic;
     const commandBody = msg.content.slice(prefix.length);
@@ -19,63 +20,111 @@ client.on('message', (msg) => {
     const command = args.shift().toLowerCase();
 
     if(command === 'entrylist'){
-        msg.channel.send("**Loading list, please wait...**");
-        setTimeout(function(){
-            //msg.channel.send(channelID);
-            axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
-                params: {
-                action: 'entryList',
-                eventID:channelID
-                }
-            }).then(function (response) {
-                msg.channel.bulkDelete(50);
-                msg.channel.send(response.data.entrylist,{split:true});
-            });
-        },3000);
-    }  
-
-    else if(command === 'checkin'){
-        globalEventID = channelID   
-        if(channelID){
-        axios.get('http://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
+        axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
             params: {
-                action: 'checkin',
-                discordID: driver_username,
-                eventID:channelID
+              action: 'entryList',
+              eventID:channelID
             }
         }).then(function (response) {
-            msg.channel.send("!process");
+            msg.channel.send(response.data.entrylist,{split:true});
         }).catch(function (error) {
             console.log(error);
-          }); 
-        }
-    } 
-    
-    else if(command === 'checkout'){
-        globalEventID = channelID   
-        if(channelID){
-        axios.get('http://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
-            params: {
-                action: 'checkout',
-                discordID: driver_username,
-                eventID:channelID
-            }
-        }).then(function (response) {
-            msg.channel.send("!process");
-        }).catch(function (error) {
-            console.log(error);
-          }); 
-        }
-    } 
-
-    else if(command === 'process'){
-        msg.channel.bulkDelete(50);
-        msg.channel.send('!entrylist'); 
+        }); 
     }
 
+    else if(command === 'checkin'){
+        globalEventID = channelID
+        if(channelID){
+        axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
+            params: {
+              action: 'checkin',
+              discordID: driver_username,
+              eventID:channelID
+            }
+        }).then(function (response) {
+            //msg.channel.send(response.data.message);
+            //setTimeout(function(){ 
+            //    msg.channel.send("!delete");
+           //},5000); //time in milliseconds
+            msg.channel.send("!delete");
+            console.log("ok");
+        }).catch(function (error) {
+            console.log(error);
+          }); 
+        }//end of if eventID
+    }
+    
+    else if(command === 'checkout'){
+        globalEventID = channelID
+        if(channelID){
+        axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
+            params: {
+              action: 'checkout',
+              discordID: driver_username,
+              eventID:channelID
+            }
+        }).then(function (response) {
+            //msg.channel.send(response.data.message);
+            //setTimeout(function(){ 
+            //    msg.channel.send("!delete");
+           //},5000); //time in milliseconds
+            msg.channel.send("!delete");
+            console.log("ok");
+        }).catch(function (error) {
+            console.log(error);
+          }); 
+        }//end of if eventID
+    }
 
+    else if(command === 'points'){
+        globalEventID = channelID
+        if(channelID){
+        axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
+            params: {
+              action: 'pointstable',
+              discordID: driver_username,
+              eventID:channelID
+            }
+        }).then(function (response) {
+            msg.channel.send(response.data.message,{split:true});
+            console.log("ok");
+            //setTimeout(function(){ 
+                //msg.channel.send("!delete");
+           //},5000); //time in milliseconds
+        }).catch(function (error) {
+            console.log(error);
+          }); 
+        }//end of if eventID
+    }
+    
+    else if(command === 'teamregistration'){
+        globalEventID = channelID
+        if(channelID){
+        axios.get('https://ergp.axlemotorsport.com/race/assets/functions/discordBot.php', {
+            params: {
+              action: 'teamRegistration',
+              discordID: driver_username,
+              leagueID:channelID
+            }
+        }).then(function (response) {
+            msg.channel.send(response.data.message,{split:true});
+            console.log("ok");
+            //setTimeout(function(){ 
+                //msg.channel.send("!delete");
+           //},5000); //time in milliseconds
+        }).catch(function (error) {
+            console.log(error);
+          }); 
+        }//end of if eventID
+    }
+
+    else if(command === 'delete'){
+        msg.channel.messages.fetch({limit:10}).then(messages =>{
+            msg.channel.bulkDelete(messages);
+            msg.channel.send("!entrylist "+channelID);
+        });
+    }
   });
-
 
 client.login(process.env.BOT_TOKEN);
 
